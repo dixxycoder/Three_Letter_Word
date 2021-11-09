67b430b1ff8e1f07fd8f7ddce85b87187614cd02
Grant Spraker <grant.spraker@gmail.com>
2021-11-09 11:37:33 -0700

    part one done

--- Three_Letter_Word/ViewController.swift
+++ Three_Letter_Word/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    //declaring varbuls and Outlets
+    @IBOutlet weak var currentLetterLabel: UILabel!
+    
+    @IBOutlet weak var a_Label: UILabel!
+    
+    @IBOutlet weak var horanzoltleView: UIStackView!
+    
+    @IBOutlet var letterLabels: [UILabel]!
+    
+    var alphubet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+   
+    var i = 0
+    var currentLetter = "a"
+    
+    
+//    var letter1 = ""
+//    var letter2 = ""
+//    var letter3 = ""
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapLetter(_ sender: UITapGestureRecognizer)
+    {
+        currentLetter = alphubet[i]
+        
+        currentLetterLabel.text = currentLetter
+    }
+    
 }
 
